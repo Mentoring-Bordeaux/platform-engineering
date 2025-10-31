0258ef8a4ed164c21c930d46fefa62c9da38ac01
Mathias ROSA <mat.ros@outlook.fr>
2025-10-31 16:31:36 +0100

    feat(app): implement CardSelect component for platform selection

---
+++ app/app/components/CardSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <u-page-card
+    :icon="icon"
+    :title="title"
+    :class="styleClasses"
+    @click="onSelect"
+  />
+</template>
+
+<script setup lang="ts">
+const { icon, title, isSelected } = defineProps<{
+  icon: string
+  title: string
+  isSelected: boolean
+}>()
+
+const emits = defineEmits<{
+  (e: 'select', isSelected: boolean): void
+}>()
+
+const styleClasses = computed(() => {
+  const defaultClasses = 'border box-border cursor-pointer'
+  return isSelected
+    ? `${defaultClasses} border-primary`
+    : `${defaultClasses} border-transparent`
+})
+
+const onSelect = () => {
+  emits('select', isSelected)
+}
+</script>
