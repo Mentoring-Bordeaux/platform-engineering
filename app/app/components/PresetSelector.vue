2e0ffcda6f47cf6ef90c17edbccacb17c8403093
Mathias ROSA <mat.ros@outlook.fr>
2025-10-31 17:01:04 +0100

    feat(app): add framework and preset selection

--- app/app/components/PresetSelector.vue
+++ app/app/components/PresetSelector.vue
@@ -1,39 +1,59 @@
 <template>
   <FormSection title="Preset">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <u-page-card
-        @click="onPresetSelect('nuxt-dotnet')"
-        title="Nuxt + Dotnet"
-        description="A starter template with Nuxt.js frontend and Dotnet backend."
-      />
-      <u-page-card
-        @click="onPresetSelect('react-dotnet')"
-        title="React + Dotnet"
-        description="A starter template with React frontend and Dotnet backend."
-      />
-      <u-page-card
-        @click="onPresetSelect('my-awesome-template')"
-        title="My Awesome Template"
-        description="An awesome template for your my project."
-      />
-      <u-page-card
-        @click="onPresetSelect('react-nestjs')"
-        title="React + NestJS"
-        description="A starter template with React frontend and NestJS backend."
+      <CardSelect
+        v-for="(preset, key) in presets"
+        :key="key"
+        :title="preset.name"
+        :description="preset.description"
+        :is-selected="selectedPreset === key"
+        @click="onPresetSelect(key)"
       />
     </div>
     <u-separator label="or" />
-    <u-page-card
+    <CardSelect
+      :is-selected="selectedPreset === 'blank'"
       title="Blank Template"
-      description="Start from scratch with a blank template."
+      description="Use your own custom template repository."
+      @click="onPresetSelect('blank')"
     />
   </FormSection>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import FormSection from './FormSection.vue'
 
-const onPresetSelect = (preset: string) => {
-  console.log('Selected preset:', preset)
+interface Preset {
+  name: string
+  description: string
+}
+
+const presets = {
+  'nuxt-dotnet': {
+    name: 'Nuxt + Dotnet',
+    description: 'A starter template with Nuxt.js frontend and Dotnet backend.'
+  },
+  'react-dotnet': {
+    name: 'React + Dotnet',
+    description: 'A starter template with React frontend and Dotnet backend.'
+  },
+  'my-awesome-template': {
+    name: 'My Awesome Template',
+    description: 'An awesome template for your my project.'
+  },
+  'react-nestjs': {
+    name: 'React + NestJS',
+    description: 'A starter template with React frontend and NestJS backend.'
+  }
+} as const satisfies Record<string, Preset>
+
+type PresetKey = keyof typeof presets | 'blank'
+
+const selectedPreset = ref<string | null>(null)
+
+const onPresetSelect = (key: PresetKey) => {
+  // console.log('Selected preset:', presets[key].name)
+  selectedPreset.value = key
 }
 </script>
