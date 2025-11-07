dbd097337c5b88e93e690de6094d58ca73e6f48a
Mathias ROSA <mat.ros@outlook.fr>
2025-11-07 08:25:48 +0100

    feat(app): group whole form into one single component

---
+++ app/app/types/index.ts
@@ -0,0 +1,14 @@
+export interface Preset {
+  name: string
+  description: string
+}
+
+export interface Framework {
+  name: string
+  icon: string
+}
+
+export interface Platform {
+  name: string
+  icon: string
+}
