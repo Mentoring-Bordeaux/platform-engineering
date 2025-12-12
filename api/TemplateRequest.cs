38fac41701d16c5201076bcf6df641984fa9a3d4
Mathieu Chaillon <mathieu.chaillon@gmail.com>
2025-12-12 01:08:58 +0100

    feat(api): refactor Program.cs and PulumiService to permit to generalize the functionment for all the new pulumi presets chore(api) : Change the use of environment variables (github PAT and organization name) need to be add with dotnet secret feat(api): Treat the result of the creation of each resource to inform the front in response of the endpoint with data parsed and cleaned

--- api/TemplateRequest.cs
+++ api/TemplateRequest.cs
@@ -1,6 +1,8 @@
 public class TemplateRequest
 {
     public string Name { get; set; } = string.Empty;
+
+    public string ResourceType { get; set; } = string.Empty;
     public string? Framework { get; set; } = null;
     public Dictionary<string, string> Parameters { get; set; } = new();
 }
