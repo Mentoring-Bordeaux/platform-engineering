cb556e6b2111da18678319ba6f3fa924f0153159
Mathieu Chaillon <mathieu.chaillon@gmail.com>
2025-12-19 14:34:08 +0100

    chore(app): Rename type variables and comment

--- app/app/types/platforms.ts
+++ app/app/types/platforms.ts
@@ -1,13 +1,27 @@
 import type { Field } from './fields'
 
 export interface Platform {
-  platformType: string
+  /** Type of platform where the repository is hosted.
+   * @example "github"
+   */
+  type: string
   icon: string
+  /**
+   * Configuration fields for this platform.
+   */
   config: Record<string, Field>
 }
 
 export interface ConfiguredPlatform {
-  platformType: string
+  /** Type of platform where the repository is hosted.
+   * @example "github"
+   */
+  type: string
+  /** Human-friendly name for this repository
+   * @example "My GitHub Repository"
+   */
   name: string
+  /** User-provided configuration values for the created repository.
+   */
   config: Record<string, unknown>
 }
